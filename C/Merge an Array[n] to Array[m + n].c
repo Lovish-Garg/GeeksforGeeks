e7cd08dd46c8525877c6f95a2f054bb104cfd11c
Lovish Garg <70561590+Lovish-Garg@users.noreply.github.com>
2020-11-13 20:34:55 +0530

    create

---
+++ C/Merge an Array[n] to Array[m + n].c
@@ -0,0 +1,86 @@
+// In this program I have remade a program of gfg in C but in gfg it was in C++.
+#include <stdio.h>
+#define NA -1
+
+void merge(int arr[], int arr2[], int n, int m);
+void moveToEnd(int *ptr, int size);
+
+/*
+Here not any parameter means it can take any parameter otherwise it could be initialise as void print_arr(int, int);
+*/
+void print_arr();
+
+int main(void)
+{
+    int mPlusN[] = {2, 8, NA, NA, NA, 13, NA, 15, 20}; 
+    int N[] = {5, 7, 9, 25}; 
+    int n = sizeof(N)/sizeof(N[0]); 
+    int m = sizeof(mPlusN)/sizeof(mPlusN[0]) - n;
+    
+    moveToEnd(mPlusN, m + n);
+    
+    printf("\nAfter moving to end->\n");
+    print_arr(mPlusN,m + n);
+
+    merge(mPlusN, N, n, m);
+
+    printf("\n\nAfter merging the arrays->\n ");
+    print_arr(mPlusN, m + n);
+
+    printf("\n");
+
+    return 0;
+}
+
+void print_arr(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d\t", arr[i]);
+}
+
+// function to move the array data of nPlusM to right side
+void moveToEnd(int mPlusN[], int size) 
+{ 
+  int i = 0, j = size - 1; 
+
+  for (i = size-1; i >= 0; i--) 
+  {
+        if (mPlusN[i] != NA) 
+        { 
+        mPlusN[j] = mPlusN[i]; 
+      
+      /*
+         if( i != j) , if this step isnot done then duplicates in array can be produced but ignoring this block will not produce any problem in final outut as the duplicates will be over-written. You can see the duplicates in After moving to end-> result.
+      */
+        if (i != j)
+        {
+            mPlusN[i] = NA;
+        }
+
+        j--; 
+    }
+  }
+} 
+
+void merge(int mPlusN[], int N[], int n, int m)
+{
+    int i = n;// index where our first element exists in this case it is 2 and its index is 4 in mplusN array
+    int j = 0;// index for N[] array 
+    int k = 0; // index for the current index where the number needs to store and we are incrementing it in each case 
+
+    while (k < (m + n))
+    {
+        if ((i < (m + n) && mPlusN[i] <= N[j]) || (j == n))
+        {
+            mPlusN[k] = mPlusN[i];
+            k++;
+            i++; 
+        }
+        else
+        {
+            mPlusN[k] = N[j];
+            k++;
+            j++;
+        }
+    }
+}
