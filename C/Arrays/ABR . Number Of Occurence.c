63bef25de52842b22376c9507491dc48a98cb3e3
Lovish-Garg <lovishg100@gmail.com>
2020-12-19 10:55:56 +0530

    create

--- C/Arrays/ABR . Number Of Occurence.c
+++ C/Arrays/ABR . Number Of Occurence.c
@@ -1,8 +1,16 @@
 // This program is C version of Gfg problem Number Of Occurence
+/*
+	This program works by getting the index of first 
+	occurence of number x in array then getting last index
+	of occrence in array then print 
+	last - first + 1
+*/
 #include <stdio.h>
 
-int count(int [], int, int);
-int first_occurence(int[], int, int);
+int first_occur(int [], int, int);
+int last_occur(int [], int, int, int);
+void Printcount_occur(int [], int, int);
+
 int main(void)
 {
 	int t;
@@ -20,7 +28,7 @@ int main(void)
 		int arr[n];
 
 		printf("\nInput data in array->\n");
-		for (int i = 0; i < n; i++)
+		for(int i = 0; i < n; ++i)
 			scanf("%d", &arr[i]);
 
 		int num;
@@ -28,44 +36,77 @@ int main(void)
 		printf("\nNumber: ");
 		scanf("%d", &num);
 
-		int occur = count(arr, n, num);
+		Printcount_occur(arr, n, num);
 
-		(occur == 0) ? 	printf("\nNumber not found\n\n") : 
-						printf("\nOccurence: %d\n\n", occur); 
+		printf("\n");
 	}
-}
+}	
 
-int count(int arr[], int n, int x) 
+void Printcount_occur(int arr[], int n, int x)
 {
-	    int occur = 0;
-	    int index = first_occurence(arr, n, x);
-	    
-	    for (int i = index; i < n; i++)
-	        if (arr[i] == x)
-	            occur++;
-	           
-	    return occur;
+	int first = first_occur(arr, n, x);
+
+	if (first == -1)// if first contains -1 means x not found
+	{
+		printf("\n%d not found\n", x);
+		return;
+	}
+
+	int last = last_occur(arr, first, n, x);
+
+	printf("\nFirst Occurence: %d\nLast Occurence: %d\nTotal Occurence: %d\n", first, last, last - first + 1);		
 }
 
-int first_occurence(int arr[], int n, int x)// this function returns the index of first occurence where the number was found
+int first_occur(int arr[], int n, int x)
 {
 	int l = 0, r = n - 1;
-    while (l < r)
-    {
-        int mid = l + (r - l) / 2;
-        
-        if (arr[mid] == x)
-        {
-            if (mid == 0 || arr[mid - 1] != arr[mid])
-                return mid;
-            
-            r = mid - 1;
-        }
-        else if (x < arr[mid])
-        {
-            r = mid - 1;
-        }
-        else
-            l = mid + 1;
-    }
+
+	while (l <= r)
+	{
+		int mid = l + (r - l) / 2;
+
+		if (arr[mid] == x && (mid == 0 || arr[mid - 1] < x))
+			return mid;
+
+		// this else if and else part is important because
+		/*
+			if arrr[mid] == x but it isnot the first occurence then the else part
+			will get evaluated and we will search for first occurence in first part
+		*/
+		else if (x < arr[mid])
+			l = mid + 1;
+
+		else
+			r = mid - 1; 
+	}
+
+	return -1;
+}
+
+
+int last_occur(int arr[], int l, int n, int x)
+{
+	int r = n -1;
+
+	while (l <= r)
+	{
+		int mid = l + (r - l) / 2;
+
+		if (arr[mid] == x && (mid == n - 1 || arr[mid + 1] > x))
+			return mid;
+
+		// this else if and else part is also important in similar way because
+		/*
+			if arrr[mid] == x but it isnot the last occurence then the else part
+			will get evaluated and we will search for first occurence in second part
+			of array
+		*/
+		else if (x < arr[mid])
+			r = mid - 1;
+
+		else
+			l = mid + 1;
+	}
+
+	return -1;
 }
